0d78793e0c4f866fb441de96f946997c2af891fe
HeyItzGinger <league@iMac-2.attlocal.net>
2019-03-18 18:24:12 -0700

    Started RobotColorChooser

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -3,17 +3,46 @@
 
 package elseif;
 
-import javax.swing.JOptionPane;
 import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot bob = new Robot();
 		//3. Ask the user what color they would like the robot to draw
-		
+		String color = JOptionPane.showInputDialog("what color do you wish the robot to draw");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+		if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.red);
+		}else if (color.equalsIgnoreCase("blue")) {
+			bob.setPenColor(Color.blue);
+		}else if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.red);
+		}else if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.red);
+		}else if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.red);
+		}else if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.red);
+		}else if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.red);
+		}else if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.red);
+		}else if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.red);
+		}else if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.red);
+		}else if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.red);
+		}else if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.red);
+		}else if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.red);
+		}
         //6. If the user doesn’t enter anything, choose a random color
 
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
